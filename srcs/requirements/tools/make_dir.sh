137ccbc8eba092060f274d3d46e56bdcbb0eef7c
Sergio Garcia <segarcia@student42.com>
2023-10-24 06:50:25 -0400

    new version of makedir

--- srcs/requirements/tools/make_dir.sh
+++ srcs/requirements/tools/make_dir.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [[ ! -d "/home/${USER}/data" ] || [[${USER} == "root" ] && [! -d "/root/data"] ]]; then
-        mkdir ~/data
-        mkdir ~/data/mariadb
-        mkdir ~/data/wordpress
+if [[ ! -d "/home/${USER}/data" || ( ${USER} == "root" && ! -d "/root/data" ) ]]; then
+        mkdir -p /home/${USER}/data/mariadb
+        mkdir -p /home/${USER}/data/wordpress
 else
-	echo "Directories already exists"
+        echo "Directories already exist"
 fi
