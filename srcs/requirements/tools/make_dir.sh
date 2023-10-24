f8894d4984ceffe34d307f221d9815cb9304c45e
rootsegarcia <root@segarcia>
2023-10-24 05:10:15 -0400

    make dirs

--- srcs/requirements/tools/make_dir.sh
+++ srcs/requirements/tools/make_dir.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-if [ ! -d "/home/${USER}/data" ]; then
+
+if [[ ! -d "/home/${USER}/data" ] || [[${USER} == "root" ] && [! -d "/root/data"] ]]; then
         mkdir ~/data
         mkdir ~/data/mariadb
         mkdir ~/data/wordpress
+else
+	echo "Directories already exists"
 fi
